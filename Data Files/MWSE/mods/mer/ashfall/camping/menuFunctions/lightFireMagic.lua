5dcbf03f348491ba117df8d219ed05acdeafca15
GreatKingWombat <onomatopoeia171@gmail.com>
2021-06-28 01:56:37 -0400

    Magic fire lighting basic implementation complete

--- Data Files/MWSE/mods/mer/ashfall/camping/menuFunctions/lightFireMagic.lua
+++ Data Files/MWSE/mods/mer/ashfall/camping/menuFunctions/lightFireMagic.lua
@@ -10,11 +10,12 @@ return {
         )
     end, 
     callback = function(campfire)
-        tes3.playSound{ reference = tes3.player, sound = "ashfall_light_fire"  }
+        tes3.playSound{ reference = tes3.player, sound = "mysticism area"  }
         common.log:debug("Lighting Fire %s", campfire.object.id)
         tes3.playSound{ sound = "Fire", reference = campfire, loop = true }
         event.trigger("Ashfall:Campfire_Enablelight", { campfire = campfire})
         campfire.data.fuelLevel = campfire.data.fuelLevel - 0.5
+        tes3.mobilePlayer:exerciseSkill(tes3.skill.destruction, 5)
         campfire.data.isLit = true
         event.trigger("Ashfall:Campfire_Update_Visuals", { campfire = campfire, nodes = true, fire = true, lighting = true})
     end,
